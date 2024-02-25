684855278e37df96b9f4040241ca96543dd654d4
Flavio Tavares Silva <flavio.tsx@gmail.com>
2024-02-24 23:20:00 -0300

    feat: add client part

---
+++ client/client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		fmt.Println("Erro ao criar uma nova requisição", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Erro ao enviar a resposta", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erro ao ler a resposta", err)
+	}
+
+	var quotation struct {
+		Bid float64 `json:"bid"`
+	}
+
+	err = json.Unmarshal(body, &quotation)
+	if err != nil {
+		fmt.Println("Erro ao deserializar a resposta", err)
+	}
+
+	fmt.Printf("Dollar: %.2f\n", quotation.Bid)
+
+	err = os.WriteFile("cotacao.json", body, 0644)
+	if err != nil {
+		fmt.Println("Erro ao escrever a resposta", err)
+		return
+	}
+
+}
